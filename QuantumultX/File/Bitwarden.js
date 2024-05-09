bd0e31bfc1b7d4384a01a63528248ad53a054bd4
zw-95 <35022819+zw-95@users.noreply.github.com>
2024-05-10 04:25:51 +0800

    fix: Bitwarden.js

--- QuantumultX/File/Bitwarden.js
+++ QuantumultX/File/Bitwarden.js
@@ -17,11 +17,16 @@ hostname = vault.bitwarden.com
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
-
+console.log('开始')
+console.log(obj.profile)
 if (url.includes("/api/sync")) {
-  console.log(obj.profile)
-  if (obj.profile?.premium!= undefined || obj.profile?.premium != null) {
-    obj.profile?.premium = true;
+  console.log('匹配到url')
+  console.log(url)
+  if (obj.profile?.premium!= undefined && obj.profile?.premium != null) {
+    console.log('匹配到高级用户信息')
+    console.log(obj.profile.premium)
+    obj.profile.premium = true;
   }
 }
+console.log('结束')
 $done({ body: JSON.stringify(obj) });
