9351520ed6879388ca1bd9788f114b89f0b3a7d5
zw-95 <35022819+zw-95@users.noreply.github.com>
2024-05-11 17:17:26 +0800

    perf: Bitwarden.js
    去除多余代码

--- QuantumultX/File/Bitwarden.js
+++ QuantumultX/File/Bitwarden.js
@@ -17,16 +17,10 @@ hostname = vault.bitwarden.com
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
-console.log('开始')
-console.log(obj.profile)
 if (url.includes("/api/sync")) {
-  console.log('匹配到url')
-  console.log(url)
   if (obj.profile?.premium!= undefined && obj.profile?.premium != null) {
-    console.log('匹配到高级用户信息')
-    console.log(obj.profile.premium)
     obj.profile.premium = true;
   }
 }
-console.log('结束')
+console.log('bitwarden已解锁')
 $done({ body: JSON.stringify(obj) });
