c741ff36fcbd2daa9a5f46a50e40988a80686676
zw-95 <35022819+zw-95@users.noreply.github.com>
2024-11-02 23:45:20 +0800

    更新 飞兔签到

--- Feitu-DailyBonus/Checkin.js
+++ Feitu-DailyBonus/Checkin.js
@@ -18,9 +18,9 @@ QX 1.0.10+ :
 [rewrite_local]
 #飞兔云Cookie
 ^https:\/\/api-cdn.feitu.im\/ft\/gateway\/cn\/user\/getSubscribe ^GET url-and-header script-request-header https://raw.githubusercontent.com/zw-95/QxScript/master/Feitu-DailyBonus/Checkin.js
-
+^https:\/\/105.91tutu.xyz\/ft\/gateway\/cn\/user\/getSubscribe ^GET url-and-header script-request-header https://raw.githubusercontent.com/zw-95/QxScript/master/Feitu-DailyBonus/Checkin.js
 [mitm]
-hostname = api-cdn.feitu.im
+hostname = api-cdn.feitu.im,105.91tutu.xyz
 ~~~~~~~~~~~~~~~~
 */
 
